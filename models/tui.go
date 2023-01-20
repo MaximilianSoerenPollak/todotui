63d782f6f6ba5f40d56fcff293e82def3361d57b
Maximilian <74557518+MaximilianSoerenPollak@users.noreply.github.com>
2023-01-20 20:57:32 +0000

    First progress in trying out multi-input

--- models/tui.go
+++ models/tui.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"fmt"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
@@ -11,13 +11,23 @@ import (
 )
 
 type taskGroupsModel struct {
-	list      list.Model
-	textInput textinput.Model
-	state     int
+	list       list.Model
+	textInputs []textinput.Model
+	state      int
+    cursorMode textinput.CursorMode
 }
 
 func (m taskGroupsModel) Init() tea.Cmd {
 	return nil
+}
+
+func (m taskGroupsModel) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.textInputs))
+
+	for i := range m.textInputs {
+		m.textInputs[i], cmds[i] = m.textInputs[i].Update(msg)
+	}
+	return tea.Batch(cmds...)
 }
 
 func (m taskGroupsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -32,57 +42,101 @@ func (m taskGroupsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			switch keypress := msg.String(); keypress {
 			case "a":
 				m.state = 1
-				m.textInput, cmd = m.textInput.Update(msg)
+				m.textInputs, cmd = m.textInputs.updateInputs(msg)
 				return m, cmd
 			}
 		case 1:
 			switch keypress := msg.String(); keypress {
+            // Change cursorMode
+            case "ctrl+r":
+                m.cursorMode++
+                if m.cursorMode > textinput.CursorHide {
+                    m.cursorMode = textinput.CursorBlink
+                }
+                cmds := make ([]tea.Cmd, len(m.textInputs))
+                for i := range m.textInputs {
+                    cmds[i] = m.textInputs[i].SetCursorMode(m.cursorMode)
+                }
+                return m, tea.Batch(cmds...)
+            // set cofucs to the next tab
+            case "tab", "shit+tab", "enter", "up", "down":
+                s :=msg.String()
+                if s == "enter" && m.focusIndex == len(m.textInputs) {
+                    return m, tea.Quit
+                }
 			case "ctrl+c":
 				return m, tea.Quit
-			case "enter":
-				if m.state == 1 {
-					lis := m.list.Items()
-					m.list.SetItems(append(lis, types.TaskGroup{GroupTitle: m.textInput.Value()}))
-					m.state = 0
-					m.textInput.SetValue("")
+			// case "enter":
+			// 	if m.state == 1 {
+			// 		lis := m.list.Items()
+			// 		m.list.SetItems(append(lis, types.TaskGroup{GroupTitle: m.textInputs.Value()}))
+			// 		m.state = 0
+			// 		m.textInputs.SetValue("")
 				}
 			}
 
 		}
+        return m, cmd
 	}
 
-	if m.state == 1 {
-		m.textInput, cmd = m.textInput.Update(msg)
-		return m, cmd
-	}
+	// if m.state == 1 {
+	// 	m.textInputs, cmd = m.textInputs.Update(msg)
+	// 	return m, cmd
+	// }
 
-	m.list, cmd = m.list.Update(msg)
+	// m.list, cmd = m.list.Update(msg)
 
-	return m, cmd
-}
+
 
 func (m taskGroupsModel) View() string {
-
 	if m.state == 0 {
 		return m.list.View()
 	}
-	return m.textInput.View()
+
+	var b strings.Builder
+
+	for i := range m.textInputs {
+		b.WriteString(m.textInputs[i].View())
+		if i < len(m.textInputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+	return b.String()
+
+	// return m.textInputs.View()
 
 }
 
 func InitiateTaskGroupsList() taskGroupsModel {
 
 	items := []list.Item{}
-
 	for _, j := range db.MemData.TaskGroups {
 		items = append(items, j)
 	}
-	fmt.Println(items)
 	ti := textinput.New()
 	ti.Placeholder = "Pikachu"
 	ti.Focus()
 	ti.CharLimit = 156
 	ti.Width = 20
+	m := taskGroupsModel{
+		list:       list.New(items, list.NewDefaultDelegate(), 0, 0),
+		textInputs: make([]textinput.Model, 2),
+		state:      0,
+	}
+	var t textinput.Model
+	for i := range m.textInputs {
+		t = textinput.New()
 
-	return taskGroupsModel{list: list.New(items, list.NewDefaultDelegate(), 0, 0), state: 0, textInput: ti}
+		switch i {
+		case 0:
+			t.Placeholder = "TaskGroupName"
+			t.Focus()
+		case 1:
+			t.Placeholder = "Description"
+			t.Focus()
+		}
+		m.textInputs[i] = t
+	}
+
+	return m
 }
